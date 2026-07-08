bd477a3a02465ad883c4d44fda6be8caff32d00c
Ashish <ashishkrgupta.7890@gmail.com>
2026-07-08 15:12:05 +0530

    15th commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println("This is from DemoApplication-2");
         System.out.println("This is from DemoApplication-1");
         System.out.println("Commit 1");
-
+        System.out.println("Commit 2");
 
 
 
