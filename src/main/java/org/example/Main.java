0c88606be1936290c46ca294e93df424c70bf4a5
Ashish <ashishkrgupta.7890@gmail.com>
2026-07-14 11:57:32 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.printf("Hello World!");
         System.out.printf("Hello and welcome!");
-        System.out.printf("Hello Ashish!");
-        System.out.printf("Hello Ashish Kumar Gupta!");
-
-
-
-
-        System.out.println("This is from DemoApplication-2");
-        System.out.println("This is from DemoApplication-1");
-        System.out.println("Commit 1");
-        System.out.println("Commit 2");
-
-
-
     }
+
 }
